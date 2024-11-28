26bcc5e47c691053173f2af54cc182ddb6e9de05
LE LANNIER <lelannier.nicolas@gmail.com>
2024-11-28 15:07:14 +0100

    Replacement des pages dans un dosssier pages et non components

--- easy-leaves-frontend/src/app/app.routes.ts
+++ easy-leaves-frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './components/login/login.component';
-import { AdministrateurComponent } from './components/administrateur/administrateur.component';
-import { ListeCollaborateurComponent } from './components/liste-collaborateur/liste-collaborateur.component';
-import { AbsenceComponent } from './components/absence/absence.component';
-import { DeconnexionComponent } from './components/deconnexion/deconnexion.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdministrateurComponent } from './pages/administrateur/administrateur.component';
+import { ListeCollaborateurComponent } from './pages/liste-collaborateur/liste-collaborateur.component';
+import { AbsenceComponent } from './pages/absence/absence.component';
+import { DeconnexionComponent } from './pages/deconnexion/deconnexion.component';
 
 export const routes: Routes = [
   { path: '', component: AboutComponent },
