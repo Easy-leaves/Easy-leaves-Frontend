75735fd1b35e29ac33803270df8c3f1deebaccce
Josh-Newman-7 <raphael.pinson.professional@gmail.com>
2024-11-29 17:44:32 +0100

    Add Components to validate or not Absences

--- easy-leaves-frontend/src/app/pages/liste-collaborateur/liste-collaborateur.component.ts
+++ easy-leaves-frontend/src/app/pages/liste-collaborateur/liste-collaborateur.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GestionCongeComponent } from './gestion-absences/gestion-absences.component';
+
 
 @Component({
   selector: 'app-liste-collaborateur',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GestionCongeComponent],
   templateUrl: './liste-collaborateur.component.html',
   styleUrl: './liste-collaborateur.component.css'
 })
 export class ListeCollaborateurComponent {
+	// Propriété pour suivre la vue active
+	  vueActive: string = 'gestionAbsences';
 
+	  // Méthode pour changer la vue active
+	  changerVue(vue: string) {
+	    this.vueActive = vue;
+	  }
 }
