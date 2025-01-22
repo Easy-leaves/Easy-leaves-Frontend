1dcc8e8f14dd546c5f6bd567eacfbbf8c1996765
Theo Le Moal <contact@theo-le-moal.fr>
2025-01-22 15:48:53 +0100

    suppression lignes pas utiles :)

--- easy-leaves-frontend/tailwind.config.js
+++ easy-leaves-frontend/tailwind.config.js
@@ -7,15 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  safelist: [
-    'border-yellow-600',
-    'border-yellow-700',
-    'border-blue-600',
-    'border-green-700',
-    'border-red-600',
-    'border-neutral-600',
-    'border-blue-500',
-    'text-white',
-  ],
   plugins: [],
 }
