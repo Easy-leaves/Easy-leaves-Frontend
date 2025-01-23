94c3d92c2d121aabe394fe6b27cb82e2a52c0446
Josh-Newman-7 <raphael.pinson.professional@gmail.com>
2025-01-23 11:38:54 +0100

    Export List conges as component

--- easy-leaves-frontend/src/app/pages/liste-collaborateur/liste-collaborateur.component.ts
+++ easy-leaves-frontend/src/app/pages/liste-collaborateur/liste-collaborateur.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GestionCongeComponent } from './gestion-absences/gestion-absences.component';
+import { ListeCongesComponent } from './liste-conges/app-liste-conges.component';
 
 
 @Component({
   selector: 'app-liste-collaborateur',
   standalone: true,
-  imports: [CommonModule, GestionCongeComponent],
+  imports: [CommonModule, GestionCongeComponent, ListeCongesComponent],
   templateUrl: './liste-collaborateur.component.html',
   styleUrls: ['./liste-collaborateur.component.css']
 })
